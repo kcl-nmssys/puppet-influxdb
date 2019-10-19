acb462250de76bce6cb6f4f88963b534171495fb
Xand Meaden <xand@xand.uk>
2019-10-19 11:12:58 +0100

    started templating config file

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,5 +1,29 @@
 class influxdb::config (
 
 ) {
-  
+
+  service {
+    'influxdb':
+      ensure => $influxdb::service_ensure;
+  }
+
+  if $influxdb::service_ensure == 'running' {
+    Service['influxdb'] {
+      enable => true
+    }
+  } else {
+    Service['influxdb'] {
+      enable => false
+    }
+  }
+
+  file {
+    '/etc/influxdb/influxdb.conf':
+      ensure  => 'present',
+      owner   => 'root',
+      group   => 'influxdb',
+      mode    => '0640',
+      content => template('influxdb/influxdb.conf.erb'),
+      notify  => Service['influxdb'];
+  }
 }
