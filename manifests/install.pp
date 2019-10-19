8af0c49cb9ee909b843c3969ded42e5afc70a167
Xand Meaden <xand@xand.uk>
2019-10-19 10:12:25 +0100

    put these in hiera

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,12 +1,15 @@
-class influxdb::install {
+class influxdb::install (
+  String $repo_url,
+  String $repo_keyurl,
+) {
   if $::influxdb::manage_repo {
     case $facts['os']['family'] {
       'RedHat': {
         yumrepo {
           'InfluxDB':
-            baseurl  => "https://repos.influxdata.com/centos/${facts['operatingsystemmajrelease']}/${facts['architecture']}/stable",
+            baseurl  => "${repo_url}/centos/${facts['operatingsystemmajrelease']}/${facts['architecture']}/stable",
             gpgcheck => true,
-            gpgkey   => 'https://repos.influxdata.com/influxdb.key',
+            gpgkey   => $repo_keyurl,
             before   => Package['influxdb'];
         }
       }
@@ -15,12 +18,11 @@ class influxdb::install {
         $distro = downcase($facts['os']['distro']['id'])
         apt::source {
           'InfluxDB':
-            location => "https://repos.influxdata.com/${distro}",
+            location => "${repo_url}/${distro}",
             release  => $facts['os']['distro']['codename'],
             repos    => 'stable',
             key      => {
-              'id'     => '05CE15085FC09D18E99EFB22684A14CF2582E0C5',
-              'server' => 'pgp.mit.edu',
+              'source' => $repo_keyurl,
             },
             before   => Package['influxdb'];
         }
